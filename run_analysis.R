3a77f5c97ec1ff5f488fcf23c4bb908a78e98ea4
Alistair McKelvie <alistair.mckelvie@gmail.com>
2015-03-22 18:31:38 +1100

    script completed

--- run_analysis.R
+++ run_analysis.R
@@ -3,6 +3,11 @@ library(data.table)
 features <- fread("./UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
 ## Select only feature with mean() and std() in their name
 features <- features[(V2 %like% 'mean[()]') | (V2 %like% 'std[()]')]
+## Do various substitutions to replace characters in features which
+## don't behave properly as column names
+features$V2 <- gsub("[^0-9A-Za-z///' ]", ".", features$V2)
+features$V2 <-gsub("([.])\\1+", "\\1", features$V2, perl=TRUE)
+features$V2 <-gsub("\\.$", "", features$V2, perl=TRUE)
 
 ## Load x data
 x_train <- data.table(read.table("./UCI HAR Dataset/train/X_train.txt"))
@@ -27,7 +32,7 @@ library(plyr)
 activities <- as.character(y_full[,V1])
 activities <- revalue(activities, c("1"="WALKING", "2"="WALKING_UPSTAIRS",
                                     "3"="WALKING_DOWSTAIRS", "4"="SITTING",
-                                    "5"="STANDING", "6"="xLAYING"))
+                                    "5"="STANDING", "6"="LAYING"))
 
 ## Read subject numbers and join
 subjects_train <- fread("./UCI HAR Dataset/train/subject_train.txt")
@@ -37,3 +42,7 @@ subjects <- rbindlist(subjects)[,V1]
 
 ## Join activities, subjects and x data
 full_data <- cbind(subjects, activities, x_full)
+
+## Make summary table
+melted <- melt(full_data, c("subjects", "activities"))
+summarised <- dcast(subjects + activities ~ variable, data=m, mean)
